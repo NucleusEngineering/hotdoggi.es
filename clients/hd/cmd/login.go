7cfc4cf10a362a06c1b8851f4337c95348de1b13
Daniel Stamer <stamer@google.com>
2024-11-14 10:55:28 +0000

    Add clients/hd license headers
    Signed-off-by: Daniel Stamer <stamer@google.com>

--- clients/hd/cmd/login.go
+++ clients/hd/cmd/login.go
@@ -1,3 +1,17 @@
+//  Copyright 2022 Google
+
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+
+//      http://www.apache.org/licenses/LICENSE-2.0
+
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
 package cmd
 
 import (
@@ -12,9 +26,7 @@ const (
 	callbackPort = "8934"
 )
 
-var (
-	redirectURI = fmt.Sprintf("http://localhost:%s/", callbackPort)
-)
+var redirectURI = fmt.Sprintf("http://localhost:%s/", callbackPort)
 
 var loginCmd = &cobra.Command{
 	Use:   "login",
